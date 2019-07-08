a5271f915217cb69a0d8843f53fad4eb7c35987e
Alan <acasasar@everis.com>
2019-07-08 22:21:04 +0200

    [FEATURE][MONGO CONNECTION] Mongo db connection was implemented Global variables was added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const mongoose = require('mongoose');
+require('./config/db');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const router = require('./routes');
 const app = express();
+require('dotenv').config({path: 'global_variables.env'});
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 
 // habilitar las vistas ocn handlebars
 app.engine('handlebars',
@@ -14,11 +21,23 @@ app.engine('handlebars',
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRET_SEED,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
+
 app.set('view engine', 'handlebars');
 
 
 app.use('/', router());
 
 
-app.listen(5000);
+app.listen(process.env.PORT);
 
