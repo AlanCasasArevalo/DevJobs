fa59f9b2e3002dc77a7fa5454a686b13731e2b18
Alan <acasasar@everis.com>
2019-07-08 17:58:58 +0200

    [FEATURE][VIEW WAS ADDED]

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 const router = require('./routes');
 const app = express();
 
+// habilitar las vistas ocn handlebars
+app.engine('handlebars',
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'handlebars');
+
+
 app.use('/', router());
+
 
 app.listen(5000);
 
