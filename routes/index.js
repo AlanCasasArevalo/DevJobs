35066190e2b52bc085b3fe042b43c9f864484974
Alan Casas <alancasasarevalo@gmail.com>
2019-07-29 19:08:59 +0200

    Security routes were added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const homeController = require('../controllers/homeController');
 const vacancyController = require('../controllers/vacancyController');
 
 module.exports = () => {
+
     router.get ('/', (req, res) => {
         res.send('funciona')
     });
@@ -20,6 +21,76 @@ module.exports = () => {
         vacancyController.addNewVacancy
     );
 
+    router.get('*', async (req, res) => {
+        res.status(404).json({
+            result: false,
+            message: 'No existe esa direccion'
+        })
+    });
+
+    router.post('*', async (req, res) => {
+        res.status(404).json({
+            result: false,
+            message: 'No existe esa direccion'
+        })
+    });
+
+    router.put('*', async (req, res) => {
+        res.status(404).json({
+            result: false,
+            message: 'No existe esa direccion'
+        })
+    });
+
+    router.delete('*', async (req, res) => {
+        res.status(404).json({
+            result: false,
+            message: 'No existe esa direccion'
+        })
+    });
+
+    router.patch('*', async (req, res) => {
+        res.status(404).json({
+            result: false,
+            message: 'No existe esa direccion'
+        })
+    });
+
+    router.head('*', async (req, res) => {
+        res.status(404).json({
+            result: false,
+            message: 'No existe esa direccion'
+        })
+    });
+
+    router.trace('*', async (req, res) => {
+        res.status(404).json({
+            result: false,
+            message: 'No existe esa direccion'
+        })
+    });
+
+
+    router.options('*', async (req, res) => {
+        res.status(404).json({
+            result: false,
+            message: 'No existe esa direccion'
+        })
+    });
+
+    router.patch('*', async (req, res) => {
+        res.status(404).json({
+            ok: false,
+            message: 'Direccion no encontrada'
+        })
+    });
+
+    router.delete('*', async (req, res) => {
+        res.status(404).json({
+            ok: false,
+            message: 'Direccion no encontrada'
+        })
+    });
 
     return router;
 };
